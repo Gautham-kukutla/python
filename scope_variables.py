75c6f4427b94f5132777d5eab3be0df9a0ae1ca5
Gautham-kakutla <gautham.kukutla@gmail.com>
2022-03-02 14:43:44 +0000

    modified scope_variables.py

--- scope_variables.py
+++ scope_variables.py
@@ -1,8 +1,9 @@
 def my_func():
     # global x
     x = 10
-    print("Value inside function:",x)
+    print("Value inside function:", x)
+
 
 x = 20
 my_func()
-print("Value outside function:",x)
+print("Value outside function:", x)
