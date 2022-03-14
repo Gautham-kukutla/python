2366b55ff75ded2175b1ac6c0a284cb0794d067f
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-14 12:55:41 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 if [ -e "changes.txt" ]
 then
   echo "changed done"
-  git push "https://Gautham-kukutla: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@github.com/Gautham-kukutla/python.git"
+  git push "https://Gautham-kukutla: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@github.com/Gautham-kukutla/python.git"
 else
 echo "no changes"
 fi
